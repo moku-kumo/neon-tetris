a6402e6bfbc1b94254f16d448084c2e32abed24b
moku-kumo <jeonshineui@gmail.com>
2026-04-22 21:40:41 +0900

    경로 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    // PWA 경로 호환을 위해 base를 './'로 설정
     base: './',
     plugins: [react(), tailwindcss()],
     define: {
